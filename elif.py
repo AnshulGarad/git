3bf5b36e397383558bdc86fc6540ecb24963df2e
Anshul Garad <anshulgarad@888gmail.com>
2026-02-26 14:59:17 +0530

    Changed

---
+++ elif.py
@@ -0,0 +1,8 @@
+marks = 76
+if marks >= 90:
+    print("Excellent")
+elif marks >=75:
+    print("Good")
+else:
+    print("Try Hard")
+print (marks)
--- if.py
+++ if.py
@@ -1,6 +1,7 @@
 marks = 56
 if marks >= 90:
     print("Excellent")
+print ("Good student of entire batch")
 
 print(marks)
 
